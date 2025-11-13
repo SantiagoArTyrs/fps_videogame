c81c4c1ab0f3523c8cbe756e133fa2b127b39a4e
Santiago Arevalo <santiago.arevalo@campusucc.edu.co>
2025-11-13 17:58:25 -0500

    Proyecto iniciado

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using static Weapon;
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; set; }
+
+    public AudioSource shootingSoundCrossbow;
+    public AudioSource reloadingSoundCrossbow;
+
+    public AudioSource shootingSoundShotgun;
+    public AudioSource reloadingSoundShotgun;
+    public AudioSource emptyMagazineSoundCrossbow;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    public void PlayShootingSound(WeaponModel weapon)
+    {
+        switch (weapon)
+        {
+            case WeaponModel.Crossbow:
+                shootingSoundCrossbow.Play();
+                break;
+            case WeaponModel.Shotgun:
+                shootingSoundShotgun.Play();
+                break;
+        }
+    }
+
+    public void PlayReloadingSound(WeaponModel weapon)
+    {
+        switch (weapon)
+        {
+            case WeaponModel.Crossbow:
+                reloadingSoundCrossbow.Play();
+                break;
+            case WeaponModel.Shotgun:
+                reloadingSoundShotgun.Play();
+                break;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,189 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using TMPro;
+
+
+public class Weapon : MonoBehaviour
+{
+
+
+    public bool isShooting, readyToShoot;
+    bool allowReset = true;
+    public float shootingDelay = 2f;
+
+    //Burst
+    public int bulletPerBurst = 3;
+    public int burstBulletsLeft;
+
+    //Spread
+    public float spreadIntensity;
+
+    //Bullet
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30f;
+    public float bulletPrefabLifeTime = 3f; //Seconds
+
+    private Animator animator;
+
+    //Reload
+
+    public float reloadTime;
+    public int magazineSize, bulletsLeft;
+    public bool isReloading;
+
+    public enum WeaponModel
+    {
+        Crossbow,
+        Shotgun
+    }
+
+    public WeaponModel thisWeaponModel;
+
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+
+    }
+    public ShootingMode currentShootingMode;
+    private void Awake()
+    {
+        readyToShoot = true;
+        burstBulletsLeft = bulletPerBurst;
+        animator = GetComponent<Animator>();
+        bulletsLeft = magazineSize;
+    }
+    void Update()
+    {
+
+        if (bulletsLeft == 0 && isShooting)
+        {
+            SoundManager.Instance.emptyMagazineSoundCrossbow.Play();
+        }
+        if (currentShootingMode == ShootingMode.Auto)
+        {
+            //Detects if mouse is held down
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else if (currentShootingMode == ShootingMode.Single ||
+            currentShootingMode == ShootingMode.Burst)
+        {
+            //Detects single press
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && isReloading == false)
+        {
+            Reload();
+        }
+
+        if (readyToShoot && isShooting == false && isReloading == false && bulletsLeft <= 0)
+        {
+            //Reload();
+
+        }
+
+        if (readyToShoot && isShooting && bulletsLeft > 0)
+        {
+            burstBulletsLeft = bulletPerBurst;
+            FireWeapon();
+        }
+
+        if (AmmoManager.Instance.ammoDisplay != null)
+        {
+            AmmoManager.Instance.ammoDisplay.text = $"{bulletsLeft / bulletPerBurst}/{magazineSize / bulletPerBurst}";
+        }
+    }
+
+    private void FireWeapon()
+    {
+        bulletsLeft--;
+        animator.SetTrigger("RECOIL");
+
+        
+        SoundManager.Instance.PlayShootingSound(thisWeaponModel);
+
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+
+
+        // Instantiate bullet and apply force
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        //Set bullet direction
+        bullet.transform.forward = shootingDirection;
+
+        //Shoot the bullet forward
+        bullet.GetComponent<Rigidbody>().AddForce(shootingDirection * bulletVelocity, ForceMode.Impulse);
+        //Destroy bullet after certain time
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+        //Cheking if we are done shooting   
+        if (allowReset)
+        {
+            //Invoke resetShot function after delay
+            Invoke("ResetShot", shootingDelay);
+            allowReset = false;
+        }
+        if (currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1)
+        {
+            burstBulletsLeft--;
+            Invoke("FireWeapon", shootingDelay);
+        }
+
+    }
+
+    private void Reload()
+    {
+
+        
+        SoundManager.Instance.PlayReloadingSound(thisWeaponModel);
+
+        animator.SetTrigger("RELOAD");
+        isReloading = true;
+        Invoke("ReloadCompleted", reloadTime);
+    }
+
+    private void ReloadCompleted()
+    {
+        bulletsLeft = magazineSize;
+        isReloading = false;
+    }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+
+    public Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100); //Some far point
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+        float x = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+        float y = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+
+        return direction + new Vector3(x, y, 0);
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
